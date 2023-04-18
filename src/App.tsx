77aa4e2b2003b4c2ce0275724d552c9e8f1b7499
francescoMain <cesaranofrancescomain@gmail.com>
2023-04-18 11:42:03 +0200

    EsBuild Can Bundle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import * as esbuild from "esbuild-wasm";
-import { log } from "console";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 function App() {
   const ref = useRef<any>();
@@ -12,12 +12,14 @@ function App() {
       return;
     }
 
-    const result = await ref.current.transform(input, {
-      loader: "jsx",
-      target: "es2015",
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     });
 
-    setCode(result.code);
+    setCode(result.outputFiles[0].text);
   };
 
   const startService = async () => {
